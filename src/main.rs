7fc5a88ee4ad0f54a8f70bd3a250059f8eabad3d
Maxime Tricoire <maxime.tricoire@datathings.com>
2024-01-05 22:12:08 +0100

    first impl

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use std::{fs::File, result::Result};
+use std::{
+    fs::File,
+    io::{BufWriter, Write},
+    result::Result,
+    sync::mpsc::channel,
+};
 
 use anyhow::Context;
-use chunkito::Chunkit;
+use chunkit::Chunkit;
 use fxhash::FxHashMap;
 use rayon::prelude::*;
 
 fn main() -> anyhow::Result<()> {
     let chunks = Chunkit::open("../measurements.txt").context("unable to chunk file")?;
 
-    chunks.par_iter().enumerate().for_each(|(index, chunk)| {
+    let (sender, receiver) = channel();
+
+    chunks.par_iter().for_each_with(sender, |sender, chunk| {
         let data = chunks.data(*chunk);
         let mut sensors: FxHashMap<&str, Sensor> = fxhash::FxHashMap::default();
 
@@ -23,21 +30,56 @@ fn main() -> anyhow::Result<()> {
                         cnt: 1,
                         min: temp,
                         max: temp,
-                        sum: temp as f64,
+                        sum: temp,
                     });
             });
 
-        let output = File::create(format!("chunk-{index}.json"))
-            .expect("unable to create chunk result file");
-        serde_json::to_writer(output, &sensors).expect("unable to serialize result to JSON");
+        sender.send(sensors).expect("unable to send");
     });
+
+    let mut all_sensors: FxHashMap<&str, Sensor> = fxhash::FxHashMap::default();
+    for sensors in receiver.into_iter() {
+        for (name, s) in sensors {
+            all_sensors
+                .entry(name)
+                .and_modify(|sensor| sensor.merge(&s))
+                .or_insert(s);
+        }
+    }
+
+    let mut sensors: Vec<_> = all_sensors.into_iter().collect();
+    let last_index = sensors.len() - 1;
+    sensors.sort_by(|(a, _), (b, _)| a.cmp(b));
+
+    let result = File::create("results.csv").context("unable to create result.csv")?;
+    let mut writer = BufWriter::new(result);
+    writer.write_all(b"{")?;
+    for (index, (name, Sensor { min, sum, cnt, max })) in sensors.into_iter().enumerate() {
+        writer
+            .write_fmt(format_args!(
+                "{name}={min:.1}/{:.1}/{max:.1}",
+                sum / cnt as f32
+            ))
+            .context("unable to write")?;
+        if index < last_index {
+            writer.write_all(b", ")?;
+        }
+    }
+    writer.write_all(b"}")?;
 
     Ok(())
 }
 
+struct NamedSensor<'a> {
+    name: &'a str,
+    min: f32,
+    max: f32,
+    avg: f32,
+}
+
 struct Sensor {
     min: f32,
-    sum: f64,
+    sum: f32,
     cnt: usize,
     max: f32,
 }
@@ -50,8 +92,19 @@ impl Sensor {
         if temp > self.max {
             self.max = temp;
         }
-        self.sum += temp as f64;
+        self.sum += temp;
         self.cnt += 1;
+    }
+
+    fn merge(&mut self, sensor: &Sensor) {
+        if self.min > sensor.min {
+            self.min = sensor.min;
+        }
+        if self.max < sensor.max {
+            self.max = sensor.max;
+        }
+        self.sum += sensor.sum;
+        self.cnt += sensor.cnt;
     }
 }
 
@@ -74,7 +127,7 @@ impl serde::Serialize for Sensor {
         use serde::ser::SerializeStruct;
         let mut s = serializer.serialize_struct("Sensor", 4)?;
         s.serialize_field("min", &self.min)?;
-        s.serialize_field("avg", &(self.sum / self.cnt as f64))?;
+        s.serialize_field("avg", &(self.sum / self.cnt as f32))?;
         s.serialize_field("max", &self.max)?;
         s.serialize_field("count", &self.cnt)?;
         s.end()
