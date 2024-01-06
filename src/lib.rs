c01a7a2f9b24d66265baf083e4ee0e4ab51f7753
Maxime Tricoire <maxime.tricoire@datathings.com>
2024-01-06 18:21:35 +0100

    manual threading impl with more aggresive parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,134 +1,7 @@
-use std::fs::File;
-use std::path::Path;
+use std::{fs::File, path::Path};
 
 use anyhow::{Context, Error};
-use memmap2::{Mmap, MmapOptions};
-use rayon::iter::plumbing::{bridge, Producer};
-use rayon::iter::*;
-
-pub struct Chunkit {
-    mmap: Mmap,
-    chunks: ChunkIter,
-}
-
-impl Chunkit {
-    pub fn open<P>(path: P) -> Result<Self, Error>
-    where
-        P: AsRef<Path>,
-    {
-        let file = File::open(path).context("unable to open file")?;
-        let mmap = unsafe { MmapOptions::new().map(&file)? };
-        let chunks = chunk_file(&mmap).context("unable to chunk the file")?;
-
-        Ok(Self { mmap, chunks })
-    }
-
-    pub fn par_iter(&self) -> ParChunkIter<'_> {
-        self.chunks.par_iter()
-    }
-
-    pub fn data(&self, chunk: Chunk) -> &str {
-        let data = unsafe { std::str::from_utf8_unchecked(&self.mmap) };
-        &data[chunk.start..chunk.end]
-    }
-}
-
-pub struct ChunkIter {
-    chunks: Vec<Chunk>,
-    it: usize,
-}
-
-impl Iterator for ChunkIter {
-    type Item = Chunk;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.it < self.chunks.len() {
-            let chunk = self.chunks[self.it];
-            self.it += 1;
-            Some(chunk)
-        } else {
-            None
-        }
-    }
-}
-
-pub struct ParChunkIter<'a> {
-    chunks: &'a [Chunk],
-}
-
-impl<'a> ParallelIterator for ParChunkIter<'a> {
-    type Item = &'a Chunk;
-
-    fn drive_unindexed<C>(self, consumer: C) -> C::Result
-    where
-        C: rayon::iter::plumbing::UnindexedConsumer<Self::Item>,
-    {
-        bridge(self, consumer)
-    }
-
-    fn opt_len(&self) -> Option<usize> {
-        Some(<Self as IndexedParallelIterator>::len(self))
-    }
-}
-
-impl<'a> IndexedParallelIterator for ParChunkIter<'a> {
-    fn len(&self) -> usize {
-        self.chunks.len()
-    }
-
-    fn drive<C: rayon::iter::plumbing::Consumer<Self::Item>>(self, consumer: C) -> C::Result {
-        bridge(self, consumer)
-    }
-
-    fn with_producer<CB: rayon::iter::plumbing::ProducerCallback<Self::Item>>(
-        self,
-        callback: CB,
-    ) -> CB::Output {
-        callback.callback(ChunkProducer {
-            chunks: self.chunks,
-        })
-    }
-}
-
-struct ChunkProducer<'a> {
-    chunks: &'a [Chunk],
-}
-
-impl<'a> Producer for ChunkProducer<'a> {
-    type Item = &'a Chunk;
-    type IntoIter = ::std::slice::Iter<'a, Chunk>;
-
-    fn into_iter(self) -> Self::IntoIter {
-        self.chunks.iter()
-    }
-
-    fn split_at(self, index: usize) -> (Self, Self) {
-        let (left, right) = self.chunks.split_at(index);
-        (
-            ChunkProducer { chunks: left },
-            ChunkProducer { chunks: right },
-        )
-    }
-}
-
-impl<'a> From<ParChunkIter<'a>> for ChunkProducer<'a> {
-    fn from(value: ParChunkIter<'a>) -> Self {
-        Self {
-            chunks: value.chunks,
-        }
-    }
-}
-
-impl<'a> IntoParallelIterator for &'a ChunkIter {
-    type Iter = ParChunkIter<'a>;
-    type Item = &'a Chunk;
-
-    fn into_par_iter(self) -> Self::Iter {
-        ParChunkIter {
-            chunks: &self.chunks,
-        }
-    }
-}
+use memmap2::MmapOptions;
 
 /// A chunk contains lines without overlapping
 #[derive(Clone, Copy, Debug)]
@@ -139,13 +12,17 @@ pub struct Chunk {
 
 // TODO make sure we are not in an escaped LF when trying to find('\n')
 // cause right now that's a bug
-fn chunk_file(mmap: &Mmap) -> Result<ChunkIter, Error> {
-    let data = unsafe { std::str::from_utf8_unchecked(mmap) };
-
+pub fn chunk_it<P: AsRef<Path>>(path: P, nb_chunks: usize) -> Result<Vec<Chunk>, Error> {
+    let file = File::open(path).context("unable to open file")?;
+    let mmap = unsafe {
+        MmapOptions::new()
+            .map_copy_read_only(&file)
+            .context("unable to mmap the file")?
+    };
+    let data = unsafe { std::str::from_utf8_unchecked(&mmap) };
     let eof = data.len();
 
     // split the file based on the number of thread
-    let nb_chunks = num_cpus::get();
     let mut chunks = Vec::with_capacity(nb_chunks);
     let chunk_size = eof / nb_chunks;
 
@@ -176,5 +53,5 @@ fn chunk_file(mmap: &Mmap) -> Result<ChunkIter, Error> {
         }
     }
 
-    Ok(ChunkIter { chunks, it: 0 })
+    Ok(chunks)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,58 @@
-use std::{
-    fs::File,
-    io::{BufWriter, Write},
-    result::Result,
-    sync::mpsc::channel,
-};
+use std::fs::File;
+use std::io::{BufWriter, Write};
+use std::result::Result;
+use std::thread::JoinHandle;
+use std::time::Instant;
 
 use anyhow::Context;
-use chunkit::Chunkit;
+use chunkit::chunk_it;
 use fxhash::FxHashMap;
-use rayon::prelude::*;
+use memmap2::MmapOptions;
+use smol_str::SmolStr;
 
 fn main() -> anyhow::Result<()> {
-    let chunks = Chunkit::open("../measurements.txt").context("unable to chunk file")?;
+    let chunks = chunk_it("../measurements.txt", 14).context("unable to chunk the file")?;
+    eprintln!("processing {} chunks:\n{chunks:?}", chunks.len());
 
-    let (sender, receiver) = channel();
-
-    chunks.par_iter().for_each_with(sender, |sender, chunk| {
-        let data = chunks.data(*chunk);
-        let mut sensors: FxHashMap<&str, Sensor> = fxhash::FxHashMap::default();
-
-        data.lines()
-            .map(|line| line.split_once(';').unwrap())
-            .map(|(name, temp)| (name, temp.parse::<f32>().unwrap()))
-            .for_each(|(name, temp)| {
+    let handles = chunks
+        .into_iter()
+        .map(|chunk| {
+            std::thread::spawn(move || {
+                let tid = std::thread::current().id();
+                let start = Instant::now();
+                eprintln!("{tid:?} spawned");
+                let file = File::open("../measurements.txt").expect("unable to open file");
+                let buf = unsafe {
+                    MmapOptions::new()
+                        .map_copy_read_only(&file)
+                        .expect("unable to mmap the file")
+                };
+                let sensors = process_chunk(&buf[chunk.start..chunk.end]);
+                eprintln!("{tid:?} done in {:?}", start.elapsed());
                 sensors
-                    .entry(name)
-                    .and_modify(|sensor| sensor.add_temp(temp))
-                    .or_insert_with(|| Sensor {
-                        cnt: 1,
-                        min: temp,
-                        max: temp,
-                        sum: temp,
-                    });
-            });
+            })
+        })
+        .collect::<Vec<JoinHandle<_>>>();
 
-        sender.send(sensors).expect("unable to send");
-    });
-
-    let mut all_sensors: FxHashMap<&str, Sensor> = fxhash::FxHashMap::default();
-    for sensors in receiver.into_iter() {
-        for (name, s) in sensors {
-            all_sensors
-                .entry(name)
-                .and_modify(|sensor| sensor.merge(&s))
-                .or_insert(s);
-        }
+    let mut sensors = Vec::new();
+    for handle in handles {
+        let result = handle.join().expect("unable to join thread");
+        sensors.push(result);
     }
 
-    let mut sensors: Vec<_> = all_sensors.into_iter().collect();
-    let last_index = sensors.len() - 1;
-    sensors.sort_by(|(a, _), (b, _)| a.cmp(b));
+    let start = Instant::now();
+    let sensors = merge_results(sensors);
+    eprintln!("merge took {:?}", start.elapsed());
 
+    let start = Instant::now();
     let result = File::create("results.txt").context("unable to create results.txt")?;
     let mut writer = BufWriter::new(result);
     writer.write_all(b"{")?;
+    let last_index = sensors.len() - 1;
     for (index, (name, Sensor { min, sum, cnt, max })) in sensors.into_iter().enumerate() {
         writer
             .write_fmt(format_args!(
-                "{name}={min:.1}/{:.1}/{max:.1}",
+                "{name:?}={min:.1}/{:.1}/{max:.1}",
                 sum / cnt as f32
             ))
             .context("unable to write")?;
@@ -66,10 +61,69 @@ fn main() -> anyhow::Result<()> {
         }
     }
     writer.write_all(b"}")?;
+    eprintln!("writing result took {:?}", start.elapsed());
 
     Ok(())
 }
 
+fn merge_results(chunk_results: Vec<FxHashMap<SmolStr, Sensor>>) -> Vec<(SmolStr, Sensor)> {
+    let mut all_sensors: FxHashMap<SmolStr, Sensor> = fxhash::FxHashMap::default();
+    for sensors in chunk_results {
+        for (name, s) in sensors {
+            all_sensors
+                .entry(name)
+                .and_modify(|sensor| sensor.merge(&s))
+                .or_insert(s);
+        }
+    }
+    let mut sensors: Vec<_> = all_sensors.into_iter().collect();
+    sensors.sort_by(|(a, _), (b, _)| a.cmp(b));
+    sensors
+}
+
+fn process_chunk(data: &[u8]) -> FxHashMap<SmolStr, Sensor> {
+    let eof = data.len();
+
+    let mut sensors: FxHashMap<SmolStr, Sensor> = fxhash::FxHashMap::default();
+    let mut name = None;
+    let mut prev = 0;
+    let mut curr = 0;
+    loop {
+        if curr == eof {
+            break;
+        }
+        match data[curr] {
+            b';' => {
+                let text = unsafe { std::str::from_utf8_unchecked(&data[prev..curr]) };
+                // eprintln!("name=\"{text}\"");
+                name.replace(SmolStr::from(text));
+                curr += 1;
+                // moving on
+                prev = curr;
+            }
+            b'\n' => {
+                let text = unsafe { std::str::from_utf8_unchecked(&data[prev..curr]) };
+                let temp = text.parse::<f32>().unwrap();
+
+                // line completed, record it
+                sensors
+                    .entry(name.clone().unwrap())
+                    .and_modify(|s| s.add_temp(temp))
+                    .or_insert_with(|| Sensor::new(temp));
+
+                // and still increment
+                curr += 1;
+                // moving on
+                prev = curr;
+            }
+            _ => {
+                curr += 1;
+            }
+        }
+    }
+
+    sensors
+}
 struct Sensor {
     min: f32,
     sum: f32,
@@ -78,6 +132,15 @@ struct Sensor {
 }
 
 impl Sensor {
+    fn new(temp: f32) -> Self {
+        Self {
+            cnt: 1,
+            min: temp,
+            max: temp,
+            sum: temp,
+        }
+    }
+
     fn add_temp(&mut self, temp: f32) {
         if temp < self.min {
             self.min = temp;
